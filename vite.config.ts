3fb27a98e2459d326b2e142f7af2a06a9be9b7db
hojin <devbang5674@gmail.com>
2024-04-22 15:22:45 +0900

    4/22 프로젝트 시작

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@pages',
+        replacement: resolve(__dirname, 'src/pages'),
+      },
+    ],
+  },
+  plugins: [react(), tsconfigPaths()],
+});
